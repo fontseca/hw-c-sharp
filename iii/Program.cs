7befe07e868a0fca6a6b0dadbda1a31bf05ab22f
fontseca <alx.17@outlook.com>
2022-04-29 12:17:11 -0600

    add switch for showing practices

--- iii/Program.cs
+++ iii/Program.cs
@@ -8,9 +8,30 @@ namespace ThirdPractice
     {
         static void Main(string[] args)
         {
-            //new Practice1().Start();
-            //new Practice2().Start();
-            new Practice3().Start();
+            WriteLine("1) Ejercicio 1");
+            WriteLine("2) Ejercicio 2");
+            WriteLine("3) Ejercicio 3");
+            WriteLine("0) Salir");
+
+            Write("#? ");
+
+            switch (int.Parse(ReadLine()))
+            {
+                case 0: return;
+                case 1:
+                    new Practice1().Start();
+                    break;
+                case 2:
+                    new Practice2().Start();
+                    break;
+                case 3:
+                    new Practice3().Start();
+                    break;
+                default:
+                    WriteLine("Opción no es válida");
+                    break;
+
+            }
         }
     }
 }
