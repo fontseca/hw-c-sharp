b5885d11e5ae78939ad9d8ecc7c89e842669a180
fontseca <alx.17@outlook.com>
2022-04-30 19:09:05 -0600

    remove useless namespaces

--- iv/Practices/Practice6.cs
+++ iv/Practices/Practice6.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using static System.Console;
+﻿using static System.Console;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FourthPractice.Practices
 {
