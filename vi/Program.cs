87d51359fae6f65c291735b9615770dcbe13155c
fontseca <alx.17@outlook.com>
2022-05-14 16:56:24 -0600

    add sixth pracitce codebase

---
+++ vi/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using static System.Console;
+
+namespace SixthPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int opt;
+                do
+                {
+                    Clear();
+                    WriteLine("SELECT A PRACTICE:\n");
+                    WriteLine("0) Exit");
+                    WriteLine("1) Practice 1");
+                    WriteLine("2) Practice 2");
+                    WriteLine("3) Practice 3");
+
+                    Write("\n#? ");
+                    opt = int.Parse(ReadLine());
+
+                    switch (opt)
+                    {
+                        case 0: WriteLine("Bye."); return;
+                        case 1:
+                            //new Practice1().Start();
+                            ReadKey();
+                            break;
+                        case 2:
+                            //new Practice2().Start();
+                            ReadKey();
+                            break;
+                        case 3:
+                            //new Practice3().Start();
+                            ReadKey();
+                            break;
+                        case 4:
+                            //new Practice4().Start();
+                            ReadKey();
+                            break;
+                        case 5:
+                            //new Practice5().Start();
+                            ReadKey();
+                            break;
+                        default:
+                            WriteLine("Not a valid option");
+                            break;
+
+                    }
+                } while (opt != 0);
+
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+        }
+    }
+}
