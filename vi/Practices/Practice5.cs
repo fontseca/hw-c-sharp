e350acdc24c7f65841eb581ff57c8fa5f7504323
fontseca <alx.17@outlook.com>
2022-06-20 18:32:25 -0600

    feat: add 5th

--- vi/Practices/Practice5.cs
+++ vi/Practices/Practice5.cs
@@ -7,7 +7,10 @@ namespace SixthPractice.Practices
     {
         public void Start()
         {
-
+            for (double x = 1; x <= 2.1; x += 0.1)
+                WriteLine(f(x) > 0 ? $"{x}\t{Math.Round(f(x), 2)}\tPOSITIVE" : $"{x}\t{Math.Round(f(x), 2)}\tNEGATIVE");
         }
+
+        private double f(double x) => (4 * Math.Pow(x, 2)) - (16 * x) + 15;
     }
 }
